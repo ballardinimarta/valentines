4cd68feddd1a410b4d40e8bdc33f2f181d4f290c
JessiAlster <jessica.haara@alster.se>
2024-01-25 15:09:23 +0100

    Add dynamic postit board to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,40 @@
-<h1>Alster Lovebombing</h1>
+<script lang="ts">
+  import { db } from "$lib/firebase/firebase";
+  import { collection, getDocs } from "firebase/firestore";
+  import Postit from "./Postit.svelte";
+  import Logo from "./Logo.svelte";
+
+  const getQuerySnapshot = async () => {
+    return await getDocs(collection(db, "notes"));
+  };
+
+  let promise = getQuerySnapshot();
+  const randomIntFromInterval = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+</script>
+
+<h1><Logo /> Alster Lovebombing <Logo /></h1>
+<div class="postit-board">
+  {#await promise then guerySnapshot}
+    {#each guerySnapshot.docs as doc}
+      <Postit
+        name={doc.data().name}
+        text={doc.data().text}
+        tilt={randomIntFromInterval(-25, 25)}
+      />
+    {/each}
+  {/await}
+</div>
 
 <style>
   h1 {
     font-size: 3rem;
     text-align: center;
+  }
+  .postit-board {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
   }
 </style>
---
+++ src/routes/Postit.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  export let name;
+  export let text;
+  export let tilt = 0;
+  console.log(window.screen.height);
+  const rand = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+  const style = `
+    --rotation: -${rand(0, 15)}deg;
+    --color: rgb(250, ${rand(100, 200)},${rand(150, 255)} );
+    --x: ${rand(100, window.screen.width - 100)}px;
+    --y: ${rand(150, window.screen.height - 250)}px;
+
+  `;
+</script>
+
+<div class="postit" {style}>
+  <h3>{name}</h3>
+  <p>{text}</p>
+</div>
+
+<style>
+  .postit {
+    background-color: var(--color);
+    padding: 1rem;
+    margin: 1rem;
+    font-size: 1.5rem;
+    width: 100px;
+    height: 100px;
+    margin: 5px;
+    text-align: center;
+    box-shadow: 3px 2px 1px #0000004d;
+    transform: rotate(var(--rotation));
+    position: absolute;
+    top: var(--y);
+    left: var(--x);
+  }
+  .postit h3 {
+    font-size: 20px;
+    font-family: var(--main-font);
+    text-align: center;
+    margin: 0;
+    text-transform: capitalize;
+  }
+  .postit p {
+    font-family: var(--handwriting-font);
+    text-align: center;
+    margin: 0;
+  }
+</style>
